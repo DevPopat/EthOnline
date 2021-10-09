402ae35030a453edf6a6ed6099796962a3bad926
HumzaMKhan <humzamk@uci.edu>
2021-10-09 10:48:50 -0700

    following tutorial from YouTube, https://www.youtube.com/watch?v=wSTbBIK8qrY&t=1020s, Dapp University.

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0xfB053467340B03d01BeD7f5c57611b6a0cb39B7a',
+    '1000000000000000000000'
+  )
+};
